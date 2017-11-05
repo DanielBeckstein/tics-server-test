67eadbd31c8f6c43d441626f9ef0ff650f08e2d9
DanielBeckstein <DanielBeckstein@users.noreply.github.com>
2017-11-05 18:37:51 +0100

    added testing

---
+++ test/test.ts
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+// import 'mocha';
+
+describe('Test function', () => {
+
+  it('should return true', () => {
+    const result = "Hello"
+    expect(result).to.equal('Hello');
+  });
+
+});
